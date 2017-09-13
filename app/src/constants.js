3c4726eaf249e525b01713d2bc019cacbbe5f34b
Robin van Leeuwen <rvanleeuwen@jouwomgeving.nl>
2017-09-13 16:27:53 +0200

    Add example texts as constants

---
+++ app/src/constants.js
@@ -0,0 +1,22 @@
+export default {
+  1: {
+    text: "In principe is het dus zo dat je uit Bus 21 stapte en op weg was naar de Openbare Bibliotheek om Stijloefeningen van Raymond Queneau te lenen. Je zocht er dus zeg maar al een tijdje naar, maar het was nergens te vinden, dus daarom moest het wel in de bibliotheek te vinden zijn. En als het er toch niet is, dan ben je dus zeg maar of te vroeg vertrokken, of te laat.",
+    name: "op facebook",
+  },
+  2: {
+    text: "Op Centraal Station stap ik af van de kar die lijken ophaalt. Ik had een lift gekregen. Mijn zoon ligt in zijn wagentje te slapen. Arm ventje. Gelukkig weet hij niet dat er een lepra-epidemie heerst in Amsterdam. Zijn wagentje is hermetisch afgesloten, er hangt een zuurstoftankje aan. Hier leeft hij in, wij hopen dat er zo geen bacillen bij hem kunnen komen.",
+    name: "in magazines",
+  },
+  3: {
+    text: "Een tsunami van forenzen loopt van en naar het Centraal Station in Amsterdam. Ik ben net uit bus 21 gestapt, een prachtige, roomblanke bus 21 die ontsierd werd door foeilelijke kopvodden waaronder potentiële terroristenvrouwtjes met hun onderdrukte gezichtjes verveeld naar buiten zaten te staren. Het is dat ik mijn bodyguards bij me had, anders was de buschauffeur ongetwijfeld bestolen en gemolesteerd door het Marokkaanse straattuig dat zoals gewoonlijk de achterbanken had opgeëist.",
+    name: "harry potter",
+  },
+  4: {
+    text: "Cilindervormige blikken trommel 21, met alle delen van het oppervlak op gelijke afstand van het middelpunt om twaalf uur ’s middags. Plaats waar de trein, tram of bus even stopt om passagiers in of uit te laten stappen. Mijn kind van het mannelijk geslacht is tijdelijk buiten werking in zijn wagen waarin hij liggend wordt rondgereden. Ik beweeg me voort in een bepaalde richting langs de van wind last hebbende tijdelijke putten waarin de fundering en kelders van een gebouw worden opgetrokken naar de voor iedereen toegankelijke instelling die boeken uitleent.",
+    name: "alles van 'voeg hier een populaire schrijver'",
+  },
+  5: {
+    text: "1. Heeft de Minister het bericht op Nurks van Max Molovich gelezen waarin hij vertelt dat hij met bus 21 naar het Centraal Station gaat. Zo ja, is de Minister zich ervan bewust zijnde dat de omgeving rond het Centraal Station eruit ziet als een slagveld uit de Eerste Wereldoorlog? En is de Minister er zich van bewust dat dit ten grondslag ligt aan de Noord-Zuidlijn? Zo ja, wat denkt de minister hier aan te kunnen gaan doen?",
+    name: "latijns, snap je toch niet",
+  },
+}
