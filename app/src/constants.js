78def423743706964a99aca36388309f87a78f6c
Robin van Leeuwen <rvanleeuwen@jouwomgeving.nl>
2017-09-14 08:54:30 +0200

    Add a story to contents

--- app/src/constants.js
+++ app/src/constants.js
@@ -1,4 +1,4 @@
-export default {
+const examples = {
   1: {
     text: "In principe is het dus zo dat je uit Bus 21 stapte en op weg was naar de Openbare Bibliotheek om Stijloefeningen van Raymond Queneau te lenen. Je zocht er dus zeg maar al een tijdje naar, maar het was nergens te vinden, dus daarom moest het wel in de bibliotheek te vinden zijn. En als het er toch niet is, dan ben je dus zeg maar of te vroeg vertrokken, of te laat.",
     name: "op facebook",
@@ -19,4 +19,14 @@ export default {
     text: "1. Heeft de Minister het bericht op Nurks van Max Molovich gelezen waarin hij vertelt dat hij met bus 21 naar het Centraal Station gaat. Zo ja, is de Minister zich ervan bewust zijnde dat de omgeving rond het Centraal Station eruit ziet als een slagveld uit de Eerste Wereldoorlog? En is de Minister er zich van bewust dat dit ten grondslag ligt aan de Noord-Zuidlijn? Zo ja, wat denkt de minister hier aan te kunnen gaan doen?",
     name: "latijns, snap je toch niet",
   },
+}
+
+const copy = {
+  title: "Stijloefeningen (25) – Lepra",
+  body: "Op Centraal Station stap ik af van de kar die lijken ophaalt. Ik had een lift gekregen. Mijn zoon ligt in zijn wagentje te slapen. Arm ventje. Gelukkig weet hij niet dat er een lepra-epidemie heerst in Amsterdam. Zijn wagentje is hermetisch afgesloten, er hangt een zuurstoftankje aan. Hier leeft hij in, wij hopen dat er zo geen bacillen bij hem kunnen komen. \n Overal op het stationsplein zitten melaatsen uitgeblust tegen een hekje of slepen ze zich voort richting nergens. De bouwputten liggen er verlaten bij. Wat heeft het nog voor zin om te bouwen? De regen maakt m’n ogen nat. Door de zweren kan ik m’n oogleden niet meer dichtdoen. Het water werkt verlichtend. Ik ben op weg naar de Openbare Bibliotheek om Stijloefeningen van Queneau te lenen. Ik ben al een tijdje naar het boek op zoek, maar kan het niet vinden. \n Voor de bibliotheek word ik staande gehouden door een melaatse vrouw. Haar gezicht ziet eruit alsof het van plastic is gemaakt en iemand er een aansteker onder heeft gehouden. Ze  vraagt in het Engels met een Frans accent of dit het postkantoor is. Ze heeft haar handen omwikkeld met zwachtels die al een tijdje niet gewassen zijn. Ze houdt een briefje vast waarop twee adressen vermeld staan. Een van de adressen is de Raadhuisstraat. Ik vertel dat ze daar moet zijn. Ze zucht en draait zich om. Achter haar zit een man in een rolstoel. Het gezicht van de man is bijna niet meer herkenbaar als gezicht. Een soort pompoen in verre staat van ontbinding. \n Binnen sta ik een klein kwartier voor de Q. Queneau is vrij ruim vertegenwoordigd, maar Stijloefeningen zit er niet bij. De bibliotheek is vrijwel uitgestorven. Ik krab achter mijn oor en sta het volgende moment met mijn oor in mijn handen. Ik doe de oor in mijn zak en strompel naar de computer. Volgens de online database van de Openbare Bibliotheek is er wel degelijk nog een exemplaar van Stijloefeningen aanwezig. Ik vraag aan de bochelaar die achter de balie staat of hij met me mee wil kijken. Samen kijken we nog een keer. Maar niks. Uit het gerochel van de bochelaar meen ik op te maken dat ze bij het magazijn nog wel een exemplaar h ebben. Ik ga naar het magazijn en ram iets te hard op de bel van de balie. De pink van mijn rechterhand, een van mijn favoriete vingers, rolt van de balie, via de bureaustoel, op de grond. Een oud vrouwtje komt, met een dubbelloops tussen mijn ogen gericht, vragen wat ik moet. Er is nog wel een exemplaar van Stijloefeningen aanwezig, weet ze me na even zoeken te melden, maar die ligt op de tweede verdieping. Waar ik net de hele middag had gezocht. Op het Bos en Lommerplein koop ik een paar babyschoentjes voor mijn zoontje. Hopelijk heeft hij z’n voetjes nog als dit voorbij is.",
+}
+
+export {
+  examples as default,
+  copy,
 }
