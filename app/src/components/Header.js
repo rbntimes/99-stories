b66b337840c7b17ec6f5cba5f9815432ad18db4a
Robin van Leeuwen <rvanleeuwen@jouwomgeving.nl>
2017-09-14 08:54:41 +0200

    Create header component

---
+++ app/src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import NsLogo from './../components/NsLogo';
+import H3 from './../components/H3';
+
+const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 1rem;
+  background-color: rgba(0,48,130,.8);
+  box-shadow: 0 0.125rem 0 0 rgba(7,7,33,.15);
+`;
+
+export default function() {
+  return (
+    <Header>
+      <NsLogo color="#fff" />
+      <H3 color="white">
+        x nurks
+      </H3>
+    </Header>
+  )
+}
