40617b88a65a82e7b0902db4e5296d226fdc1097
Robin van Leeuwen <rvanleeuwen@jouwomgeving.nl>
2017-09-17 15:06:02 +0200

    Move router to index.js

--- app/src/index.js
+++ app/src/index.js
@@ -1,10 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import storiesApp from './reducers';
+import { Switch, Route } from 'react-router-dom';
+import Setup from './pages/setup';
 
-ReactDOM.render((
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-), document.getElementById('root'));
+import Main from './pages/main';
+import Register from './pages/register';
+import Login from './pages/login';
+import Article from './pages/article';
+import styled from 'styled-components';
+import Header from './components/Header';
+import Body from './pages';
+const Container = styled.div`
+  padding: .4375rem;
+  height: 90vh;
+  font-family: 'Open Sans', sans-serif;
+`;
+
+let store = createStore(
+  storiesApp,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Setup} />
+          <Route path="/register" component={Register} />
+          <Route path="/login" component={Login} />
+          <Route exact path="/articles" component={Main} />
+          <Route exact path="/articles/:niveau" component={Main} />
+          <Route path="/articles/:niveau/:article" component={Article} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+);
