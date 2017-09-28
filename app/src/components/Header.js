704b3c4f0acd4825f9a7dea862365438ed165bd0
Robin van Leeuwen <rvanleeuwen@jouwomgeving.nl>
2017-09-28 12:02:26 +0200

    Check if logged_in in Header component

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 import NsLogo from './../components/NsLogo';
 import H3 from './../components/H3';
-
+import fire from './../fire';
 import './style.css';
 
-export default () =>
+export default ({ loggedIn }) =>
   <header>
     <Link to="/">
       <NsLogo color="#fff" />
     </Link>
-    <Link to="/register">Registreren</Link>/
-    <Link to="/login">Inloggen</Link>
+    {!loggedIn
+      ? <div>
+          <Link to="/register">Registreren</Link>
+          <Link to="/login">Inloggen</Link>
+        </div>
+      : <button onClick={() => fire.auth().signOut()}> Logout </button>}
     <H3 color="white">x nurks</H3>
   </header>;
