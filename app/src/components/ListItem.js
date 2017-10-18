1ee6df1b5f0f22e3fed4227835ef7c9a714a8173
Robin van Leeuwen <rvanleeuwen@jouwomgeving.nl>
2017-10-18 13:38:03 +0200

    Wip - Remove aside from main, add filter and sorting to blocks on main page

--- app/src/components/ListItem.js
+++ app/src/components/ListItem.js
@@ -9,33 +9,20 @@ export default function({
   annotationsCount,
   timesRead,
   read,
+  display,
 }) {
-  if (type === 'large') {
-    return (
-      <li key={key}>
-        <article>
-          <Link to={`articles/${article.slug}`}>
-            <h1>{article.title}</h1>
-          </Link>
-          <p>{article.body.substr(0, 500)}</p>
-        </article>
-      </li>
-    );
-  } else {
-    return (
-      <li key={key}>
-        <article>
-          <Link to={`articles/${article.slug}`}>
-            <h4>{article.title}</h4>
-          </Link>
-          <div>
-            <span>{timesRead}x gelezen</span>
-            <span>{read ? 'Gelezen' : 'Ongelezen'}</span>
-            <span>{annotationsCount} annotaties</span>
-          </div>
-          {/* <p>{article.body.substr(0, 100)}</p> */}
-        </article>
-      </li>
-    );
-  }
+  return (
+    <li key={key}>
+      <article>
+        <Link to={`articles/${article.slug}`}>
+          <h3>{article.title}</h3>
+        </Link>
+        <footer>
+          <span>{timesRead}x gelezen</span>
+          <span>{annotationsCount}x geannoteerd</span>
+        </footer>
+        <p>{article.body.substr(0, 500)}</p>
+      </article>
+    </li>
+  );
 }
--- app/src/pages/main.js
+++ app/src/pages/main.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ListItem from './../components/ListItem';
 import getOr from 'lodash/fp/getOr';
+import { Link } from 'react-router-dom';
 
 import { articles } from './../constants';
 import fire from './../fire';
@@ -14,6 +15,7 @@ class Main extends Component {
       commentsLoaded: false,
       sort: 'read',
       hideRead: false,
+      display: 'row',
     };
 
     this.onSelect = this.onSelect.bind(this);
@@ -72,13 +74,86 @@ class Main extends Component {
       hideRead,
       sort,
       userRead,
+      display,
     } = this.state;
-    console.log(this.state);
+
+    const randomStory = articles[Math.floor(Math.random() * articles.length)];
+
     return [
+      <section>
+        <div>
+          <h3>
+            <Link to={`/articles/${randomStory.slug}`}>
+              {randomStory.title}
+            </Link>
+          </h3>
+          <p>{randomStory.body.substr(0, 200)}</p>
+        </div>
+      </section>,
       <main>
-        <ul>
-          <section>
-            <h2>Artikelen </h2>
+        <section>
+          <ul>
+            <li>
+              <h3>Meest gelezen</h3>
+              <ul>
+                {articlesLoaded &&
+                  articles
+                    .sort((a, b) => {
+                      return (
+                        getOr(0, [b.slug], articleData).read -
+                        getOr(0, [a.slug], articleData).read
+                      );
+                    })
+                    .map((article, index) => {
+                      if (index > 4) {
+                        return;
+                      }
+                      return (
+                        <li>
+                          <Link to={`articles/${article.slug}`}>
+                            {article.title}
+                          </Link>
+                        </li>
+                      );
+                    })}
+              </ul>
+            </li>
+            <li>
+              <h3>Meeste comments</h3>
+              <ul>
+                {articlesLoaded &&
+                  articles
+                    .sort((a, b) => {
+                      return (
+                        Object.keys(
+                          getOr({ comments: [] }, [b.slug], articleData)
+                            .comments
+                        ).length -
+                        Object.keys(
+                          getOr({ comments: [] }, [a.slug], articleData)
+                            .comments
+                        ).length
+                      );
+                    })
+                    .map((article, index) => {
+                      if (index > 4) {
+                        return;
+                      }
+                      return (
+                        <li>
+                          <Link to={`articles/${article.slug}`}>
+                            {article.title}
+                          </Link>
+                        </li>
+                      );
+                    })}
+              </ul>
+            </li>
+          </ul>
+        </section>
+        <section>
+          <h2>Artikelen </h2>
+          <div>
             <label>
               <input
                 checked={hideRead}
@@ -87,46 +162,34 @@ class Main extends Component {
               />
               Verberg gelezen
             </label>
-          </section>
-          {shuffled &&
-            shuffled
-              .filter(article => {
-                if (hideRead) {
-                  return !getOr(false, [article.slug], userRead);
-                }
-                return article;
-              })
-              .map(article => (
-                <ListItem
-                  type="large"
-                  key={`main-${article.slug}`}
-                  article={article}
-                />
-              ))}
-        </ul>
-      </main>,
-      <aside>
-        <section>
-          <select onChange={this.onSelect}>
-            <option disabled>Sorteer op</option>
-            <option selected={sort === 'read'} value="read">
-              Aantal x gelezen
-            </option>
-            <option selected={sort === 'annotations'} value="annotations">
-              Aantal annotaties
-            </option>
-            <option selected={sort === 'ascending'} value="ascending">
-              1 - 99
-            </option>
-            <option selected={sort === 'descending'} value="descending">
-              99 - 1
-            </option>
-          </select>
-        </section>
-        <section>
+            <label>
+              Filter op
+              <select onChange={this.onSelect}>
+                <option disabled>Sorteer op</option>
+                <option selected={sort === 'read'} value="read">
+                  Aantal x gelezen
+                </option>
+                <option selected={sort === 'annotations'} value="annotations">
+                  Aantal annotaties
+                </option>
+                <option selected={sort === 'ascending'} value="ascending">
+                  1 - 99
+                </option>
+                <option selected={sort === 'descending'} value="descending">
+                  99 - 1
+                </option>
+              </select>
+            </label>
+          </div>
           <ul>
-            {articlesLoaded &&
-              articles
+            {shuffled &&
+              shuffled
+                .filter(article => {
+                  if (hideRead) {
+                    return !getOr(false, [article.slug], userRead);
+                  }
+                  return article;
+                })
                 .sort((a, b) => {
                   if (sort === 'read') {
                     return (
@@ -165,15 +228,84 @@ class Main extends Component {
                     timesRead={
                       getOr({ read: 0 }, [article.slug], articleData).read
                     }
-                    read={getOr(false, [article.slug], userRead)}
-                    type="small"
-                    key={`aside-${article.slug}`}
+                    display={display}
+                    type="large"
+                    key={`main-${article.slug}`}
                     article={article}
                   />
                 ))}
           </ul>
         </section>
-      </aside>,
+      </main>,
+      // <aside>
+      //   <section>
+      // <select onChange={this.onSelect}>
+      //   <option disabled>Sorteer op</option>
+      //   <option selected={sort === 'read'} value="read">
+      //     Aantal x gelezen
+      //   </option>
+      //   <option selected={sort === 'annotations'} value="annotations">
+      //     Aantal annotaties
+      //   </option>
+      //   <option selected={sort === 'ascending'} value="ascending">
+      //     1 - 99
+      //   </option>
+      //   <option selected={sort === 'descending'} value="descending">
+      //     99 - 1
+      //   </option>
+      // </select>
+      //   </section>
+      //   <section>
+      //     <ul>
+      //       {articlesLoaded &&
+      //         articles
+      // .sort((a, b) => {
+      //   if (sort === 'read') {
+      //     return (
+      //       getOr(0, [b.slug], articleData).read -
+      //       getOr(0, [a.slug], articleData).read
+      //     );
+      //   } else if (sort === 'annotations') {
+      //     return (
+      //       Object.keys(
+      //         getOr({ comments: [] }, [b.slug], articleData).comments
+      //       ).length -
+      //       Object.keys(
+      //         getOr({ comments: [] }, [a.slug], articleData).comments
+      //       ).length
+      //     );
+      //   } else if (sort === 'ascending') {
+      //     return (
+      //       Math.round(a.slug.split('-')[1]) -
+      //       Math.round(b.slug.split('-')[1])
+      //     );
+      //   } else {
+      //     return (
+      //       Math.round(b.slug.split('-')[1]) -
+      //       Math.round(a.slug.split('-')[1])
+      //     );
+      //   }
+      // })
+      //           .map(article => (
+      //             <ListItem
+      // annotationsCount={
+      //   Object.keys(
+      //     getOr({ comments: [] }, [article.slug], articleData)
+      //       .comments
+      //   ).length
+      // }
+      // timesRead={
+      //   getOr({ read: 0 }, [article.slug], articleData).read
+      // }
+      //               read={getOr(false, [article.slug], userRead)}
+      //               type="small"
+      //               key={`aside-${article.slug}`}
+      //               article={article}
+      //             />
+      //           ))}
+      //     </ul>
+      //   </section>
+      // </aside>,
     ];
   }
 }
