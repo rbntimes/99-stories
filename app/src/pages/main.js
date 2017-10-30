8cd16e642d69bbe153f172b7e819d3b634910dec
Robin van Leeuwen <rvanleeuwen@jouwomgeving.nl>
2017-10-30 18:49:02 +0100

    Add emptyslate

--- app/src/pages/main.js
+++ app/src/pages/main.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ListItem from './../components/ListItem';
+import EmptySlate from './../components/EmptySlate';
 import getOr from 'lodash/fp/getOr';
 import { Link } from 'react-router-dom';
 
@@ -91,181 +92,191 @@ class Main extends Component {
           <Link to={`/articles/${randomStory.slug}`}>Lees meer</Link>
         </div>
       </section>,
-      <main>
-        <section>
-          <ul>
-            <li>
-              <h3>Meest gelezen</h3>
-              <ul>
-                {articlesLoaded &&
-                  articles
-                    .sort((a, b) => {
-                      return (
-                        getOr(0, [b.slug], articleData).read -
-                        getOr(0, [a.slug], articleData).read
-                      );
-                    })
-                    .map((article, index) => {
-                      if (index > 4) {
-                        return false;
-                      }
-                      return (
-                        <li>
-                          <Link to={`articles/${article.slug}`}>
-                            {article.title}
-                          </Link>
-                        </li>
-                      );
-                    })}
-              </ul>
-            </li>
-            <li>
-              <h3>Meeste comments</h3>
-              <ul>
-                {articlesLoaded &&
-                  articles
-                    .sort((a, b) => {
-                      return (
-                        Object.keys(
-                          getOr({ comments: [] }, [b.slug], articleData)
-                            .comments
-                        ).length -
-                        Object.keys(
-                          getOr({ comments: [] }, [a.slug], articleData)
-                            .comments
-                        ).length
-                      );
-                    })
-                    .map((article, index) => {
-                      if (index > 4) {
-                        return false;
-                      }
-                      return (
-                        <li>
-                          <Link to={`articles/${article.slug}`}>
-                            {article.title}
-                          </Link>
-                        </li>
-                      );
-                    })}
-              </ul>
-            </li>
-          </ul>
-        </section>
-        <section>
-          <h2>Artikelen </h2>
-          {this.props.user && (
-            <div>
-              <label>
-                <input
-                  checked={hideRead}
-                  onChange={this.toggleRead}
-                  type="checkbox"
-                />
-                Verberg gelezen
-              </label>
-              <label>
-                Filter op
-                <select value={sort} onChange={this.onSelect}>
-                  <option disabled>Sorteer op</option>
-                  <option value="read">Aantal x gelezen</option>
-                  <option value="annotations">Aantal annotaties</option>
-                  <option value="ascending">1 - 99</option>
-                  <option value="descending">99 - 1</option>
-                </select>
-              </label>
-            </div>
-          )}
-          {console.log(this.props, this.state)}
-          {this.props.user ? (
+      articlesLoaded && (
+        <main>
+          <section>
             <ul>
-              {this.props.userIsLoggedIn &&
-              (Object.keys(getOr([], 'userRead', this.state)).length !==
-                articles.length ||
-                !this.state.hideRead) &&
-              shuffled ? (
-                shuffled
-                  .filter(article => {
-                    if (hideRead) {
-                      return !getOr(false, [article.slug], userRead);
-                    }
-                    return article;
-                  })
-                  .sort((a, b) => {
-                    if (sort === 'read') {
+              <li>
+                <h3>Meest gelezen</h3>
+                <ul>
+                  {articles
+                    .sort((a, b) => {
                       return (
                         getOr(0, [b.slug], articleData).read -
                         getOr(0, [a.slug], articleData).read
                       );
-                    } else if (sort === 'annotations') {
+                    })
+                    .map((article, index) => {
+                      if (index > 4) {
+                        return false;
+                      }
                       return (
-                        Object.keys(
-                          getOr({ comments: [] }, [b.slug], articleData)
-                            .comments
-                        ).length -
-                        Object.keys(
-                          getOr({ comments: [] }, [a.slug], articleData)
-                            .comments
-                        ).length
+                        <li>
+                          <Link to={`articles/${article.slug}`}>
+                            {article.title}
+                          </Link>
+                        </li>
                       );
-                    } else if (sort === 'ascending') {
-                      return (
-                        Math.round(a.slug.split('-')[1]) -
-                        Math.round(b.slug.split('-')[1])
-                      );
-                    } else {
-                      return (
-                        Math.round(b.slug.split('-')[1]) -
-                        Math.round(a.slug.split('-')[1])
-                      );
-                    }
-                  })
-                  .map(article => {
-                    return (
-                      <ListItem
-                        annotationsCount={
+                    })}
+                </ul>
+              </li>
+              <li>
+                <h3>Meeste comments</h3>
+                <ul>
+                  {articlesLoaded &&
+                    articles
+                      .sort((a, b) => {
+                        return (
                           Object.keys(
-                            getOr({ comments: [] }, [article.slug], articleData)
+                            getOr({ comments: [] }, [b.slug], articleData)
+                              .comments
+                          ).length -
+                          Object.keys(
+                            getOr({ comments: [] }, [a.slug], articleData)
                               .comments
                           ).length
+                        );
+                      })
+                      .map((article, index) => {
+                        if (index > 4) {
+                          return false;
                         }
-                        timesRead={
-                          getOr({ read: 0 }, [article.slug], articleData).read
-                        }
-                        type="large"
-                        key={`main-${article.slug}`}
-                        article={article}
-                      />
-                    );
-                  })
-              ) : (
-                <span>JE HEBT ALLES GELEZEN.. SICK</span>
-              )}
+                        return (
+                          <li>
+                            <Link to={`articles/${article.slug}`}>
+                              {article.title}
+                            </Link>
+                          </li>
+                        );
+                      })}
+                </ul>
+              </li>
             </ul>
-          ) : (
-            <ul>
-              {shuffled.map(article => {
-                return (
-                  <ListItem
-                    annotationsCount={
-                      Object.keys(
-                        getOr({ comments: [] }, [article.slug], articleData)
-                          .comments
-                      ).length
-                    }
-                    timesRead={
-                      getOr({ read: 0 }, [article.slug], articleData).read
-                    }
-                    type="large"
-                    key={`main-${article.slug}`}
-                    article={article}
+          </section>
+          <section>
+            <div>
+              <h2>Artikelen </h2>
+              <span>
+                {this.state.hideRead &&
+                  `${articles.filter(article => {
+                    return getOr(false, [article.slug], userRead);
+                  }).length} verhalen verborgen`}
+              </span>
+            </div>
+            {this.props.user && (
+              <div>
+                <label>
+                  <input
+                    checked={hideRead}
+                    onChange={this.toggleRead}
+                    type="checkbox"
                   />
-                );
-              })}
-            </ul>
-          )}
-        </section>
-      </main>,
+                  Verberg gelezen
+                </label>
+                <label>
+                  Sorteer op
+                  <select value={sort} onChange={this.onSelect}>
+                    <option disabled>Sorteer op</option>
+                    <option value="random">Random</option>
+                    <option value="read">Meest gelezen</option>
+                    <option value="annotations">Aantal annotaties</option>
+                    <option value="ascending">1 - 99</option>
+                    <option value="descending">99 - 1</option>
+                  </select>
+                </label>
+              </div>
+            )}
+            {this.props.user ? (
+              <ul>
+                {this.props.userIsLoggedIn &&
+                articlesLoaded &&
+                (Object.keys(getOr([], 'userRead', this.state)).length !==
+                  articles.length ||
+                  !this.state.hideRead) &&
+                shuffled ? (
+                  shuffled
+                    .sort((a, b) => {
+                      if (sort === 'read') {
+                        return (
+                          getOr(0, [b.slug], articleData).read -
+                          getOr(0, [a.slug], articleData).read
+                        );
+                      } else if (sort === 'annotations') {
+                        return (
+                          Object.keys(
+                            getOr({ comments: [] }, [b.slug], articleData)
+                              .comments
+                          ).length -
+                          Object.keys(
+                            getOr({ comments: [] }, [a.slug], articleData)
+                              .comments
+                          ).length
+                        );
+                      } else if (sort === 'ascending') {
+                        return (
+                          Math.round(a.slug.split('-')[1]) -
+                          Math.round(b.slug.split('-')[1])
+                        );
+                      } else if (sort === 'descending') {
+                        return (
+                          Math.round(b.slug.split('-')[1]) -
+                          Math.round(a.slug.split('-')[1])
+                        );
+                      }
+                      return true;
+                    })
+                    .map(article => {
+                      return (
+                        <ListItem
+                          hideRead={hideRead}
+                          read={getOr(false, [article.slug], userRead)}
+                          annotationsCount={
+                            Object.keys(
+                              getOr(
+                                { comments: [] },
+                                [article.slug],
+                                articleData
+                              ).comments
+                            ).length
+                          }
+                          timesRead={
+                            getOr({ read: 0 }, [article.slug], articleData).read
+                          }
+                          type="large"
+                          key={`main-${article.slug}`}
+                          article={article}
+                        />
+                      );
+                    })
+                ) : (
+                  <EmptySlate />
+                )}
+              </ul>
+            ) : (
+              <ul>
+                {shuffled.map(article => {
+                  return (
+                    <ListItem
+                      annotationsCount={
+                        Object.keys(
+                          getOr({ comments: [] }, [article.slug], articleData)
+                            .comments
+                        ).length
+                      }
+                      timesRead={
+                        getOr({ read: 0 }, [article.slug], articleData).read
+                      }
+                      type="large"
+                      key={`main-${article.slug}`}
+                      article={article}
+                    />
+                  );
+                })}
+              </ul>
+            )}
+          </section>
+        </main>
+      ),
     ];
   }
 }
